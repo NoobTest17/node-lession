f2e65e9c125394b30f7d7cd1f14e15b879421eb8
NoobTest17 <maximnnn.nnn@yandex.ru>
2023-02-05 20:31:17 +0300

    init

---
+++ test/index.js
@@ -0,0 +1,21 @@
+// import {calc} from './calc'
+import {readline} from "readline"
+import { readFileSync,  } from 'fs'
+
+const rl = readline.createInterface(
+  {
+    input: process.stdin,
+    output: process.stdout
+  }
+)
+
+r1.question()
+
+//
+// console.log(calc)
+// process.stdin.on('data', (d) => {
+//   const data = d.toString();
+//   const str = data.split(' ');
+//   console.log('вы ввели ----->', data);
+//   console.log('вы ввели ----->', data);
+// })
